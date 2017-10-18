c744cb8987f737330f53c960e203ca483bf6f324
Mresquilin <32654787+Mresquilin@users.noreply.github.com>
2017-10-17 21:49:33 -0400

    Finished into .js and set up user profile in invoice project.

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+
+
+var user = {
+    name: 'Erick Esquilin',
+    companyName: 'TechLaunch.io',
+    address: '8400 NW 36th St #450, Doral, FL 33166',
+    email: 'Eesquilin@techlaunch.io',
+    picture: 'Spiderman.jpg'
+}
+
+document.querySelector('.name').textContent = user.name;
+document.querySelector('.company_name').textContent = user.company_name;
+document.querySelector('.address').textContent = user.address;
+document.querySelector('.email').textContent = user.email;
---
+++ intro.js/intro.js
@@ -0,0 +1,77 @@
+
+// Exercise 1
+var op1 = 12 + 12;
+var op2 = 36 - 12;
+var op3 = 6 * 4;
+var op4 = 48/2;
+
+console.log(op1);
+console.log(op2);
+console.log(op3);
+console.log(op4);
+
+
+for(var i=0; i< 10; i++){
+    if(i%4==0){
+    console.log("i is currently "+i);
+
+    }
+}
+
+// Exercise 2
+
+var ope1= 6 %3;
+var ope2 = 10 % 2;
+var ope3 = 5 % 3;
+
+console.log("if op1 (" + ope1+ ") is greater than op2("+ope2+") than celebrate!")
+console.log(ope1 !== ope2)
+console.log(ope1 != ope3)
+
+//Exercise 3
+var Student1 = "Student 1";
+var Testgrades = "Test Grades as follows:";
+var com = "Comment: ''None''";
+var ninesix = 96;
+
+console.log(Testgrades)
+console.log(Student1+":96")
+console.log(com)
+
+
+
+// Exercise 4
+
+var numbers=[0,1,2,3,4,5,6,7,8,9];
+
+console.log(numbers[3] + numbers[5])
+
+//Exercise 5
+
+var student ={
+    Firstname: "Erick",
+    Lastname: "Esquilin",
+    Age: "35",
+    Language: "English",
+    Ethnicity: "Hispanic"
+}
+
+    
+
+
+
+//Exercise 10
+    console.log(student.Age);
+
+var num = 1;
+    for (num = 0;num <10; num++){
+        if(num%2 == 1){
+            console.log(num);
+        }
+    }
+
+
+
+
+
+
